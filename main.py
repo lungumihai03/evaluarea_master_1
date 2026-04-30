2685faa52b9420300fe1c360983b9bbe74907bc1
Mihai Lungu <lungu.mihai03@gmail.com>
2026-04-30 16:22:40 +0300

    modified in line 8 in main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def is_valid_message(msg):
     return isinstance(msg, str) and len(msg) > 0
 
 if __name__ == "__main__":
-    print("2 + 3 =", add(2, 3))
+    print("3 + 6 =", add(3, 6))
     print("Valid:", is_valid_message("hello"))
